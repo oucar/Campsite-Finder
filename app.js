c0f698761dc3eb60e1d2ee6bed17dfa4ffbcd0fb
Onur Ucar <onurucar@MacBook-Pro-4.attlocal.net>
2022-02-08 05:24:40 -0500

    cloudinary

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// DOTENV
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const path = require('path');
