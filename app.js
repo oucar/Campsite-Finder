dc1ead1732789b798261bf9f2c46afb09cd83e25
Onur Ucar <onurucar@MacBook-Pro-4.attlocal.net>
2022-02-08 05:10:49 -0500

    added multer and it works :D

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const ExpressError = require('./utils/ExpressError');
 
 // Models
 const Campground = require('./models/campground');
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,8 +15,9 @@ const campgroundController = require('../controllers/campgrounds');
 
 
 // Multer - uploading images
+// Cloudinary to save files in the cloud
 const multer = require('multer');
-const upload = multer({dest: '/uploads'});
+const upload = multer({dest: '../uploads'});
 
 // ! #### ROUTES #### 
 // ? We can also use route.route !
@@ -26,7 +27,11 @@ router.get('/', catchAsync(campgroundController.index));
 
 // ! NEW
 router.get('/new', isLoggedIn, campgroundController.newGet);
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgroundController.newPost));
+// router.post('/', isLoggedIn, validateCampground, catchAsync(campgroundController.newPost));
+router.post('/', upload.array('campground[image]'), (req, res) =>{
+    console.log(req.body, req.files);
+    res.send("IT WORKED.")
+})
 
 // ! SHOW
 router.get('/:id', catchAsync(campgroundController.showGet));
