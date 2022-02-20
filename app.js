5c0dfa7e98fca85f0eefe343e012c1b4cdd303d9
Onur Ucar <onurucar@MacBook-Pro-4.attlocal.net>
2022-02-20 14:15:18 -0500

    fixed some bugs that occured after adding helmet

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // DOTENV
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
@@ -14,6 +14,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const ExpressError = require('./utils/ExpressError');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 // Models
 const Campground = require('./models/campground');
@@ -22,7 +23,9 @@ const User = require('./models/user');
 
 // mongoose
 const mongoose = require('mongoose');
-const { exists } = require('./models/campground');
+const {
+    exists
+} = require('./models/campground');
 mongoose.connect('mongodb://localhost:27017/campgrounds', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -87,6 +90,78 @@ passport.deserializeUser(User.deserializeUser());
 // mongo sanitize
 app.use(mongoSanitize());
 
+//  helmet
+// app.use(
+//     helmet({
+//         crossOriginEmbedderPolicy: false,
+//         // ...
+//     })
+// );
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/ajax/",
+    "https://cdn.jsdelivr.net",
+    "https://code.jquery.com",
+    "https://ajax.googleapis.com/ajax/",
+
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+
+
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [
+    "https://cdnjs.cloudflare.com/ajax/",
+    "https://thumbs.gfycat.com/"
+
+];
+app.use(
+    helmet({
+        crossOriginEmbedderPolicy: false,
+        // ...
+    }),
+    
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["http://localhost:3000/", "https://thumbs.gfycat.com/"],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dj0mmzypj",
+                "https://images.unsplash.com/",
+                "https://thumbs.gfycat.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    }),
+
+);
+
+
 
 // middleware
 // ! in every request we will have access to success flash under the key success in locals
@@ -131,9 +206,13 @@ app.all('*', (req, res, next) => {
 // ! Generic Error Handler
 // if page does not exist, next() will hit this error handler
 app.use((err, req, res, next) => {
-    const {statusCode = 500 } = err;
-    if(!err.message) err.message = 'Something went wrong.';
-    res.render('error.ejs', {err});
+    const {
+        statusCode = 500
+    } = err;
+    if (!err.message) err.message = 'Something went wrong.';
+    res.render('error.ejs', {
+        err
+    });
     // res.send(`${message} - ERROR CODE: ${statusCode}`);
 })
 
